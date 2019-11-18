e766bc9b419cf0de71d788ea7b7b5872c244c9eb
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-11-18 15:58:45 +0700

    Commit exercise 5 tapi belum fix harus di revisi

---
+++ exercises-5.js
@@ -0,0 +1,21 @@
+function palindrome(word){
+    var reverseWord = ''
+    
+    for(i = word.length - 1; i >= 0; i--){
+        reverseWord += word[i]
+    }
+
+    if(reverseWord === word){
+        return true
+    }else {
+        return false
+    }    
+
+}
+
+// TEST CASES
+console.log(palindrome('katak')); // true
+console.log(palindrome('blanket')); // false
+console.log(palindrome('civic')); // true
+console.log(palindrome('kasur rusak')); // true
+console.log(palindrome('mister')); // false
