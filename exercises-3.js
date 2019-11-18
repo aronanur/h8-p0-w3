7e0d7e2aa20a9b43f7d50149509e44b6a0c0d2fe
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-11-18 15:08:57 +0700

    Commit Exercise 3

---
+++ exercises-3.js
@@ -0,0 +1,19 @@
+var input = [
+    ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"],
+    ["0002", "Dika Sembiring", "Medan", "10/10/1992", "Bermain Gitar"],
+    ["0003", "Winona", "Ambon", "25/12/1965", "Memasak"],
+    ["0004", "Bintang Senjaya", "Martapura", "6/4/1970", "Berkebun"]
+]
+
+function dataHandling(input){
+var result = ''
+
+    for(i = 0; i < input.length; i++){
+        result += `Nomor ID : ${input[i][0]} \nNama Lengkap : ${input[i][1]} \nTTL : ${input[i][2]}, ${input[i][3]} \nHobi : ${input[i][4]} \n\n`
+    }
+
+    return result
+    
+}
+
+console.log(dataHandling(input))
