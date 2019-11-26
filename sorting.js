2eb18a3d9741a0e14e93e2ddbbfe00693ec034be
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-11-26 10:38:20 +0700

    Edit sorting

--- sorting.js
+++ sorting.js
@@ -1,6 +1,8 @@
 function sorting(arr) {
     var temp  = ''
 
+    //untuk descending atau ascending bisa di tentuin berdasakan di operator if kalo desc '<' kalo asc '>'
+
     for(i = 0; i < arr.length; i++){
         for(j = 0; j < arr.length - 1; j++){
             if(arr[j] > arr[j + 1]){
@@ -12,7 +14,6 @@ function sorting(arr) {
     }
 
     return arr
-
 }
 
 console.log(sorting(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
