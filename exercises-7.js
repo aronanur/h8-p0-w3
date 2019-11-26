48327ddef3dce224bf5a422600d9ff227ed3cfd5
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-11-27 05:37:49 +0700

    Commit perubahan exercises 7

--- exercises-7.js
+++ exercises-7.js
@@ -1,20 +1,19 @@
 function hitungJumlahKata(kalimat) { 
 
   var sentence = []
-  var tempString = ''
   var iteration = 0
 
   //mencoba tanpa built in function
   for(i = 0; i < kalimat.length; i++){
-
+    var tempString = ''
     if(kalimat[i] === ' '){
       i > 0 ? iteration++ : ''
-      tempString = ''
+      tempString = false
     }else{
       tempString += kalimat[i]
     }
     
-    if(tempString !== ''){
+    if(tempString){
       sentence[iteration] = tempString
     }
 
@@ -25,7 +24,7 @@ function hitungJumlahKata(kalimat) {
 }
   
   // TEST CASES
-  console.log(hitungJumlahKata(' I have a dream ')); // 4
+  console.log(hitungJumlahKata('I have a dream')); // 4
   console.log(hitungJumlahKata('Never eat shredded wheat or cake')); // 6
   console.log(hitungJumlahKata('A song to sing')); // 4
   console.log(hitungJumlahKata('I')); // 1
