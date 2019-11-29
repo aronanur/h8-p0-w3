12715b62a889224094b4f9d7b3d4e9fedeb43ded
Arona Nur Tetulis <arona.nur.tetulis@gmail.com>
2019-11-29 22:06:46 +0700

    Coba cara baru

--- exercises-13.js
+++ exercises-13.js
@@ -1,27 +1,40 @@
 function targetTerdekat(arr) {
-    var positionX = ''
-    var positionO = ''
-    var result = 0
+    var tempJarak = []
+    var x = []
+    var o = []
 
     for(i = 0; i < arr.length; i++){
-
-        if(arr[i] === 'o'){
-            positionO = i
-        }else if(arr[i] === 'x'){
-            positionX = i
+        if(arr[i] === 'x'){
+            x.push(i)
+        }else if(arr[i] === 'o'){
+            o.push(i)
         }
+    }
 
+    if(x.length === 0){
+        return 0
+    }
 
-        if(positionX !== '' && positionO !== ''){
-            result = Math.abs(positionO - positionX)
-            return result
+    for(i = 0; i < o.length; i++){
+        var range = 0        
+        for(j = 0; j < x.length; j++){
+            range = Math.abs(o[i] - x[j])
+            tempJarak.push(range)
+        }
+    }
+
+    var result = tempJarak[0]
+    for(i = 1; i < tempJarak.length; i++){
+        
+        if(tempJarak[i] <= result){
+            result = tempJarak[i]
         }
 
     }
 
     return result
 }
-  
+
   // TEST CASES
   console.log(targetTerdekat([' ', ' ', 'o', ' ', ' ', 'x', ' ', 'x'])); // 3
   console.log(targetTerdekat(['o', ' ', ' ', ' ', 'x', 'x', 'x'])); // 4
